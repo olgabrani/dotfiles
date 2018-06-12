aed6bb2546944ee04d1a6effd34dd38ee785f222
Olga Brani <olgabrani78@gmail.com>
2018-06-12 12:34:26 +0300

    Use setup.sh to automate setup in new VM

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cp .vimrc ~/
+cp .tmux.conf ~/
+cp .gitignore ~/
+cp .gitconfig ~/
+mkdir ~/.vim
+mkdir ~/.vim/bundle
+cp -r .vim/colors ~/.vim/
+apt-get install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
