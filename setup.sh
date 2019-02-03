6bb71e3ae0fda64eda9b0b4109f2488302b6270c
Olga Brani <olgabrani78@gmail.com>
2019-02-03 12:50:07 +0200

    setup: Add instruction to install curl

--- setup.sh
+++ setup.sh
@@ -6,6 +6,6 @@ cp .gitconfig ~/
 mkdir ~/.vim
 mkdir ~/.vim/bundle
 cp -r .vim/colors ~/.vim/
-apt-get install tmux zsh
+apt-get install tmux zsh curl
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
